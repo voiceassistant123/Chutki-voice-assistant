0b4ebc369901013d4f959f4504accb14cc065360
voiceassistant123 <amankumar2006x@gmail.com>
2026-01-28 09:28:46 +0530

    Create build.gradle.kts

---
+++ security/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = "com.chutuki.security"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
